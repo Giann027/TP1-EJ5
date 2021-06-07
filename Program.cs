b4038c55ea05202b08c9d5aacbc72473d4546274
ricot <ricot@DESKTOP-FTHKA14>
2021-06-07 17:41:54 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TP1_EJ5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Buenas!");
+            Console.Title = "Convertir de Pesos a Dolares ";
+            double DolarParalelo, DolarOficial, Pesos;
+            Console.Write("Ingrese la cantidad de pesos: ");
+            Pesos = Convert.ToSingle(Console.ReadLine());
+            DolarParalelo = Pesos / 146;
+            DolarOficial = Pesos / 93.33;
+            Console.WriteLine($"{Pesos} Pesos equivalen a {DolarParalelo} Dolares Paralelos y {DolarOficial} Dolares Oficiales");
+
+        }
+    }
+}
